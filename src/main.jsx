0bf9d6ea6fa90739f9387e28908e3c166e7becf8
dmychel <dmychel@tutanota.com>
2024-07-05 16:53:26 -0400

    removed all routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import Terminal from "./Terminal";
-import NormieView from "./NormieView";
-import Menu from "./components/terminal/Menu";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "terminal",
-    element: <Terminal />,
-  },
-  {
-    path: "normie",
-    element: <NormieView />,
-  },
-  {
-    path: "menu",
-    element: <Menu/>
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
